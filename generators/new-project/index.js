97aa5cafed355de20b029b3fa827fc168f5983b0
Jeramy Wenserit <jeramy@digidip.net>
2019-12-10 11:34:39 +0100

    Add package.json temeplate

--- generators/new-project/index.js
+++ generators/new-project/index.js
@@ -7,12 +7,20 @@ module.exports = class extends Generator {
     async projectName() {
         this.log('Creating a new Tapjaw Project');
 
-        this.args = await this.prompt({
-            name: 'projectName',
-            type: 'input',
-            message: 'Enter the name of your new project (us hyphen case, e.g. "my-test")',
-            require: true,
-        });
+        this.args = await this.prompt([
+            {
+                name: 'projectName',
+                type: 'input',
+                message: 'Enter the name of your new project (us hyphen case, e.g. "my-test")',
+                require: true,
+            },
+            {
+                name: 'author',
+                type: 'input',
+                message: 'Enter author name:',
+                require: true,
+            }
+        ]);
         this.args.root = this.contextRoot + '/' + this.args.projectName;
 
         this.log(`Creating project tree at ${chalk.greenBright(this.args.root)}`);
@@ -22,29 +30,28 @@ module.exports = class extends Generator {
         fs.mkdirSync(this.args.root);
         this.fs.copy(
             this.templatePath('src/**/.*'),
-            this.destinationPath(this.args.projectName + '/src')
+            this.destinationPath(this.args.projectName + '/src'),
         );
         this.fs.copy(
             this.templatePath('bin/*'),
             this.destinationPath(this.args.projectName + '/bin')
         );
 
+        this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json'),
+            {
+                name: this.args.projectName,
+                description: `${this.args.projectName} description`,
+                author: this.args.author,
+            }
+        )
+
         // download required files.
         const gitignore = await request.get('https://raw.githubusercontent.com/digidip/tapjaw-example/master/.gitignore');
         this.fs.write(
             this.destinationPath(this.args.projectName + '/.gitignore'),
             gitignore
-        );
-
-        const packageJson = await request.get('https://raw.githubusercontent.com/digidip/tapjaw-example/master/package.json');
-        this.fs.write(
-            this.destinationPath(this.args.projectName + '/package.json'),
-            packageJson
-                .replace('"name": "example",', `"name": "${this.args.projectName}",`)
-                .replace('"description": "Example of the Tapjaw implementation",', `"description": "${this.args.projectName} description",`)
-                .replace('"homepage": "https://github.com/digidip/example",\n', '')
-                .replace('"repository": "digidip/example",\n', '')
-                .replace('"bugs": "https://github.com/digidip/example/issues",\n', '')
         );
 
         const tsconfig = await request.get('https://raw.githubusercontent.com/digidip/tapjaw-example/master/tsconfig.json');
